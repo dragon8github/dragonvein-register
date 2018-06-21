99c6bd07136e91149ab95e8c6d2efdf5204b4ac2
dragon8github <928532756@qq.com>
2018-06-21 15:18:25 +0800

    添加了代理的测试代码

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 // npm init -y && npm i request request-promise mkdirp
 const request = require('request');
+const ipProxyRequest = require('ip-proxy-request')
+var http = require('http')
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 var path = require('path');
 var os = require('os');
+
+// user-agents
+const userAgents = [
+  'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+  'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
+  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
+  'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
+  'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0) ,Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+  'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+  'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)',
+  'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
+  'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre',
+  'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52',
+  'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+  'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)',
+  'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+  'Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6',
+  'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)',
+  'Opera/9.25 (Windows NT 5.1; U; en), Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+]
 
 // 账号
 const user = 'dragon8yima';
@@ -16,6 +41,15 @@ const token = '00637696d3e8935e40426aca722249c7a7c60048';
 
 // 项目名
 const itemid = '10438';
+
+// 获取ip列表
+const getiplist = (cb) => {
+    fs.readFile(path.join(__dirname,'/proxys.json'), {encoding:'utf-8'} , function (err, data) {
+        if(err) throw err;
+        cb && cb(JSON.parse(data)[0])
+    });
+}
+
 
 // 获取结果
 const getData = (str) => ~str.indexOf('|') ? str.split('|')[1] : str
@@ -89,31 +123,77 @@ const getsms = (mobile) => {
 
 // 第四步：注册
 const register = (mobile, code, pwd = '12345678', share = 'ec19c0ca') => {
-    request({
-        method: 'POST',
-        uri: 'http://guss.one/api/api/user/register',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest'
-        },
-        body: JSON.stringify({
-            'Phone': mobile,
-            'Code': code,
-            'Pwd': pwd,
-            'Share': share
+
+
+    // request({
+    //     method: 'GET',
+    //     url: 'http://ip.chinaz.com/getip.aspx',
+    //     timeout: 8000,
+    //     encoding: null,
+    //     proxy: 'http://91.205.239.120:8080'
+    // }, function (err, _res, body) {
+    //     if (err) throw new Error(err)
+    //     body = body.toString();
+    //     console.log(body);
+    // })
+
+    // request({
+    //     method: 'POST',
+    //     uri: 'http://192.168.0.102',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'X-Requested-With': 'XMLHttpRequest',
+    //         'User-Agent': userAgents
+    //     },
+    //     proxy: 'http://91.205.239.120:8080',
+    //     timeout: 30000,
+    //     followRedirect: true,
+    //     followAllRedirects: true,
+    //     body: JSON.stringify({
+    //         'Phone': mobile,
+    //         'Code': code,
+    //         'Pwd': pwd,
+    //         'Share': share
+    //     })
+    // }, function (err, _res, body) {
+    //     if (err) throw new Error(err)
+    //     console.log(body);
+    // })
+   
+    // getiplist(ip => {
+        
+        request({
+            method: 'POST',
+            uri: 'http://guss.one/api/api/user/register',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            proxy: 'http://91.205.239.120:8080',
+            timeout: 30000,
+            body: JSON.stringify({
+                'Phone': mobile,
+                'Code': code,
+                'Pwd': pwd,
+                'Share': share
+            })
+        }, function (err, _res, body) {
+            if (err) throw new Error(err)
+            if (~body.indexOf('注册异常') || ~body.indexOf('无效用户')) {
+                console.log("注册失败", body);
+            } else {
+                console.log("注册成功", body);
+                save(`${mobile} ———— ${pwd}`);
+            }
         })
-    }, function (err, _res, body) {
-        if (err) throw new Error(err)
-        if (~body.indexOf('注册异常')) {
-            console.log("注册失败", body);
-        } else {
-            console.log("注册成功", body);
-            save(`${mobile} ———— ${pwd}`);
-        }
-    })
+    // })
 };
 
+// for (var i = 0; i < 10; i++) {
+//     getmobile();
+// }
 
-getmobile();
+
+ getmobile();
 
 
